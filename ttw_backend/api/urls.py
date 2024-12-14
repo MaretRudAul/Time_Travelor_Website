61829fc12709800a14004ce5049c6de256402ce0
MaretRudAul <mrudinaulenbach@gmail.com>
2024-12-14 00:12:34 -0500

    Initialized Django backend, requirements.txt

---
+++ ttw_backend/api/urls.py
@@ -0,0 +1,7 @@
+# api/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('generate-code/', views.generate_code, name='generate_code'),
+]
---
+++ ttw_backend/api/views.py
@@ -0,0 +1,14 @@
+# api/views.py
+from django.http import JsonResponse
+import secrets
+
+def generate_code(request):
+    charset = (
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789"
+        "!@#$%^&*()_+~`|}{[]:;?><,./-="
+    )
+    length = 60
+    secure_code = "".join(secrets.choice(charset) for _ in range(length))
+    return JsonResponse({"secure_code": secure_code})
